c7200a36876b26d8dc4ec93a8e6e7508c5389c5c
Atabek <ikramkenjegulov91@gmail.com>
2024-08-16 17:42:10 +0500

    added personal data states

---
+++ states/__init__.py
@@ -0,0 +1,2 @@
+from .personal_data import PersonalData
+
---
+++ states/personal_data.py
@@ -0,0 +1,6 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class PersonalData(StatesGroup):
+    fullname = State()
+    phone = State()
+    age = State()
